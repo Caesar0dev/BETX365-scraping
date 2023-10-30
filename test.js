8e2f9a5c7eea817c4f7e930ece0123bafff6f127
devboss0924 <smallstar0924@gmail.com>
2023-10-30 17:56:20 -0500

    modified

--- test.js
+++ test.js
@@ -8,8 +8,6 @@ const io = require('socket.io-client');
 async function handleScraping() {
     async function launchBrowser() {
 
-        let Digital_Token = null;
-
         const browser = await puppeteer.launch({
             headless: false, // Use the new Headless mode
             // ... other options
@@ -17,36 +15,6 @@ async function handleScraping() {
 
         // Rest of your code using the browser instance
         const page = await browser.newPage();
-
-        // // Enable request interception
-        // await page.setRequestInterception(true);
-
-        // page.on('request', async (request) => {
-        //     console.log("request!");
-        //     const url = request.url();
-        //     if (url === 'wss://premiumfancy.centralredis.in/socket.io/?EIO=4&transport=websocket') {
-        //       const socketId = request.headers()['sec-websocket-key'];
-        //       console.log(`Socket ID: ${socketId}`);
-        //     }
-        //     request.continue()
-        // });
-
-        // page.on('websocket', (ws) => {
-        //     console.log("websocket!");
-        //     const url = ws.url();
-        //     if (url.includes('wss://premiumfancy.centralredis.in/socket.io/?EIO=4&transport=websocket') ) {
-        //         const socketId = ws._socket._handle._parent._peername.port;
-        //         console.log(`Socket ID: ${socketId}`);
-                
-        //         ws.on('message', (data) => {
-        //         console.log(`Socket Message: ${data}`);
-        //         });
-        //     }
-        
-        //     // Continue with WebSocket connection
-        //     ws.continue();
-        // });
-        
 
         // Navigate to a page that triggers AJAX requests
         await page.goto('https://betx365.win/exchange/Match/Inner/4/32743986', {
@@ -116,67 +84,12 @@ async function handleScraping() {
             } catch (error) {
                 console.log(error);
             }
-                // console.log('response: ', response);
-                
-                // fs.writeFileSync('result.json', response);
-                
 
         };
       
         cdp.on('Network.webSocketFrameReceived', printResponse); // Fired when WebSocket message is received.
         cdp.on('Network.webSocketFrameSent', printResponse); // Fired when WebSocket message is sent.
 
-        // fetch("wss://premiumfancy.centralredis.in/socket.io/?EIO=4&transport=websocket", {
-        //     "headers": {
-        //         "accept-language": "en-US,en;q=0.9,ko;q=0.8",
-        //         "cache-control": "no-cache",
-        //         "pragma": "no-cache",
-        //         "sec-websocket-extensions": "permessage-deflate; client_max_window_bits",
-        //         "sec-websocket-key": "KehE7v02vAseJgYikvwGfA==",
-        //         "sec-websocket-version": "13"
-        //     },
-        //     "body": null,
-        //     "method": "GET"
-        // })
-        //     .then(message => {
-        //         if (message.ok) {
-        //             console.log("Okkkkkkkkkkkkkkkkkkkkkkkkkk");
-        //             return response.json(); // assuming the response is in JSON format
-        //         } else {
-        //             throw new Error("Request failed with status " + response.status);
-        //         }
-        //     })
-        //     .then(data => {
-        //         // handle the response data here
-        //         console.log("-----------------------------------------------------");
-        //         console.log(data);
-        //         console.log("-----------------------------------------------------");
-        //         // const infos = data.vehicleSummaryList;
-        //         // for (let num = 0; num < infos.length; num++) {
-        //         //     const info = infos[num];
-        //         //     if (info.payLaterRate) {
-        //         //         carName = info.make;
-        //         //         carDesc = info.makeModel;
-        //         //         payLaterAmount = info.payLaterRate.amount;
-        //         //         payLaterTotalAmount = info.payLaterRate.totalRateAmount;
-        //         //         countNum = countNum + 1;
-        //         //         csvRow[num] = {'Code': searchKey, 'Name': carName, 'Type': carDesc, 'PayLater': payLaterAmount, 'PayLaterTotal': payLaterTotalAmount, 'State': state, 'City': city, 'Full Location': fullLocation, 'URL': link}
-        //         //         writeData.push(csvRow[num]);
-
-        //         //         // Write data to CSV file
-        //         //         csvWriter
-        //         //             .writeRecords(writeData)
-        //         //             .then(() => console.log('CSV file written successfully.'))
-        //         //             .catch((err) => console.error('Error writing CSV file:', err));
-        //         //     }
-        //         // }
-        //     })
-        //     .catch(error => {
-        //         // handle any errors that occurred during the request
-        //         console.error(error);
-        //     });
-
-        // await browser.close();
     }
 
     await launchBrowser()
