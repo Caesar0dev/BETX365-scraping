50c5b22736d561dc32de38e5db7a6089a715f24e
devboss0924 <smallstar0924@gmail.com>
2023-10-31 07:48:40 -0500

    modified

--- demo.js
+++ demo.js
@@ -37,97 +37,104 @@ async function handleScraping() {
 
         await delay(2000); // 10,000 milliseconds = 10 seconds
         const menu = await page.waitForSelector('#menu_user_d_cr_inplay', {timeout: 300000});
+        console.log("menu=>", menu);
         await menu.click({timeout: 300000});
         console.log("menu clicked");
 
-        await delay(2000);
-        try {
-            const aTags = await page.$$eval('ul#myID li a', as => as.map(a => a.textContent));
-            console.log("Cricket Name >>> ", aTags)
-        } catch (error) {
-            console.log(error);
-        }
-
-        // await delay(30000); // 10,000 milliseconds = 10 seconds
+        // await delay(3000);
         // try {
-        //     const firstSideBarXPath = '//*[@id="data_list"]/li[2]/a';
-        //     const [firstSideBar] = await page.$x(firstSideBarXPath);
-        //     await firstSideBar.click({timeout:300000});
-        //     console.log("WBBL clicked!");
+        //     const aTags = await page.waitForSelector('#data_list li a');
+  
+        //     for (let i = 0; i < aTags.length; i++) {
+        //         const element = aTags[i];
+        //         console.log("Match Title >>> ", element);
+                
+        //     }
             
         // } catch (error) {
         //     console.log(error);
         // }
+
+        await delay(30000); // 10,000 milliseconds = 10 seconds
+        try {
+            const firstSideBarXPath = '//*[@id="data_list"]/li[1]/a';
+            const [firstSideBar] = await page.$x(firstSideBarXPath);
+            await firstSideBar.click({timeout:300000});
+            console.log("WBBL clicked!");
+            
+        } catch (error) {
+            console.log(error);
+        }
         
-        // await delay(6000); // 10,000 milliseconds = 10 seconds
-        // try {
-        //     const secondSideBarXPath = '//*[@id="data_list"]/li[1]/a';
-        //     const [secondSideBar] = await page.$x(secondSideBarXPath);
-        //     await secondSideBar.click({timeout:300000});
-        //     console.log("WBBL vs Other team clicked!");
-        // } catch (error) {
-        //     console.log(error);
-        // }
+        await delay(6000); // 10,000 milliseconds = 10 seconds
+        try {
+            const secondSideBarXPath = '//*[@id="data_list"]/li[1]/a';
+            const [secondSideBar] = await page.$x(secondSideBarXPath);
+            await secondSideBar.click({timeout:300000});
+            console.log("WBBL vs Other team clicked!");
+        } catch (error) {
+            console.log(error);
+        }
         
-        // await delay(3000); // 10,000 milliseconds = 10 seconds
-        // try {
-        //     const thirdSideBar = await page.waitForSelector('#name', {timeout: 300000});
-        //     await thirdSideBar.click({timeout: 300000});
-        //     console.log("Match odds clicked!");
+        await delay(3000); // 10,000 milliseconds = 10 seconds
+        try {
+            const thirdSideBar = await page.waitForSelector('#name', {timeout: 300000});
+            await thirdSideBar.click({timeout: 300000});
+            console.log("Match odds clicked!");
 
-        // } catch (error) {
-        //     console.log(error);
-        // }
+        } catch (error) {
+            console.log(error);
+        }
 
-        // await delay(2000); // 10,000 milliseconds = 10 seconds
-        // try {
-        //     const premiumButton = await page.waitForSelector('#showSportsBookBtn', {timeout: 300000});
-        //     await premiumButton.click({timeout: 300000});
-        //     console.log("Premium button clicked!");
+        await delay(2000); // 10,000 milliseconds = 10 seconds
+        try {
+            const premiumButton = await page.waitForSelector('#showSportsBookBtn', {timeout: 300000});
+            await premiumButton.click({timeout: 300000});
+            console.log("Premium button clicked!");
 
-        // } catch (error) {
-        //     console.log(error)
-        // }      
+        } catch (error) {
+            console.log(error)
+        }      
 
-        // await delay(6000); // 10,000 milliseconds = 10 seconds
+        await delay(6000); // 10,000 milliseconds = 10 seconds
 
-        // const cdp = await page.target().createCDPSession();
-        // await cdp.send('Network.enable');
-        // await cdp.send('Page.enable');
+        const cdp = await page.target().createCDPSession();
+        await cdp.send('Network.enable');
+        await cdp.send('Page.enable');
       
-        // const printResponse = response => {
-        //     const requestID = response.requestId;
-        //     // console.log("receive websocket!", requestID);
-        //     const socketType = response.response.payloadData;
-        //     const contentText = socketType.split("[")[1];
-        //     console.log(">>> ", contentText);
-        //     console.log('response: ', response);
-        //     try {
-        //         if (contentText.indexOf("Premium") > -1) {
-        //             console.log("###########################################################################")
-        //             const payloadData = response.response.payloadData.replace("42", "");
-        //             console.log("payload data : ", payloadData);
-        //             const data = JSON.parse(payloadData)[1].data;
-        //             console.log("data : ", data);
-        //             const json = JSON.stringify(data);
-        //             fs.writeFileSync('Demo.json', json);
-        //             console.log("###########################################################################")
+        const printResponse = response => {
+            const requestID = response.requestId;
+            // console.log("receive websocket!", requestID);
+            const socketType = response.response.payloadData;
+            const contentText = socketType.split("[")[1];
+            console.log(">>> ", contentText);
+            console.log('response: ', response);
+            try {
+                if (contentText.indexOf("Premium") > -1) {
+                    console.log("###########################################################################")
+                    const payloadData = response.response.payloadData.replace("42", "");
+                    console.log("payload data : ", payloadData);
+                    const data = JSON.parse(payloadData)[1].data;
+                    console.log("data : ", data);
+                    const json = JSON.stringify(data);
+                    fs.writeFileSync('Demo.json', json);
+                    console.log("###########################################################################")
 
-        //             // fs.appendFile("Result.json", data, function (err) {
-        //             //     if (err) throw err;
-        //             //     console.log('Writed!');
-        //             // }); 
-        //             // fs.writeFileSync('Result.json', data);
-        //         }
+                    // fs.appendFile("Result.json", data, function (err) {
+                    //     if (err) throw err;
+                    //     console.log('Writed!');
+                    // }); 
+                    // fs.writeFileSync('Result.json', data);
+                }
 
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
+            } catch (error) {
+                console.log(error);
+            }
 
-        // };
+        };
       
-        // cdp.on('Network.webSocketFrameReceived', printResponse); // Fired when WebSocket message is received.
-        // cdp.on('Network.webSocketFrameSent', printResponse); // Fired when WebSocket message is sent.
+        cdp.on('Network.webSocketFrameReceived', printResponse); // Fired when WebSocket message is received.
+        cdp.on('Network.webSocketFrameSent', printResponse); // Fired when WebSocket message is sent.
 
     }
 
--- write.js
+++ write.js
@@ -1,12 +1,32 @@
-const fs = require('fs');
+var fs = require('fs');
+// var json2csv = require('json2csv');
+var newLine = '\r\n';
+
+var fields = ['Total', 'Name'];
 
 
-const data = "Hello world!!!";
-setInterval(() => {
-    fs.writeFileSync('Test1.json', data);
-    fs.appendFile("Test2.json", data, function (err) {
-        if (err) throw err;
-        console.log('Changed!');
+var toCsv = '{ data: appendThis, fields: fields, header: false, }';
+
+fs.stat('file.csv', function (err, stat) {
+  if (err == null) {
+    console.log('File exists');
+
+    //write the actual data and end with newline
+    // var csv = json2csv(toCsv) + newLine;
+    const csv = toCsv + newLine;
+
+    fs.appendFile('file.csv', csv, function (err) {
+      if (err) throw err;
+      console.log('The "data to append" was appended to file!');
     });
-    console.log("write!");
-}, 1000)
+  } else {
+    //write the headers and newline
+    console.log('New file, just writing headers');
+    fields = fields + newLine;
+
+    fs.writeFile('file.csv', fields, function (err) {
+      if (err) throw err;
+      console.log('file saved');
+    });
+  }
+});
