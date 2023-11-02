738ce49967b2c631403c6bc8482fd2e24f9fb653
devboss0924 <smallstar0924@gmail.com>
2023-11-02 13:32:52 -0700

    demo2 modifiy

--- demo2.js
+++ demo2.js
@@ -18,7 +18,7 @@ async function handleScraping() {
         const page = await browser.newPage();
         
           // Navigate to a page that triggers AJAX requests
-          await page.goto('https://www.skyexch.art/exchange/member/fullMarket?eventType=4&eventId=32744713&marketId=1.220200073', {
+          await page.goto('https://www.skyexch.art/exchange/member/fullMarket?eventType=4&eventId=32758571&marketId=1.220452339', {
               timeout: 300000
           });
           console.log("page loaded!");
@@ -30,7 +30,7 @@ async function handleScraping() {
             const url = response.url();
 
             // Check if the response URL matches the desired URL
-            if (url === 'https://lt-fn-cdn001.akamaized.net/techablesoftware/en/Etc:UTC/gismo/match_info/41946121') {
+            if (url === 'https://lt-fn-cdn001.akamaized.net/common/en/Etc:UTC/cricket/get_scorecard/44022034') {
                 const responseBody = await response.text();
                 console.log(`Response URL: ${url}`);
                 console.log(`Response body: ${responseBody}`);
